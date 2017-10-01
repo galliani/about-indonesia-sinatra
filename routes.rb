60ff42415164d6c89577d9180f99e85639697cff
Galih Muhammad <galih0muhammad@gmail.com>
2017-10-01 08:59:30 +0700

    add simple authorization header validation

--- routes.rb
+++ routes.rb
@@ -1,18 +1,27 @@
 current_dir = Dir.pwd
 Dir["#{current_dir}/models/*.rb"].each { |file| require file }
 
+
 before do
   content_type :json
 end
 
+
 namespace '/v1' do
+  before  do 
+    is_authorized = request.env['HTTP_AUTHORIZATION'] == ENV['API_SECRET']
+  
+    halt 401 unless is_authorized
+  end
+
   namespace '/provinces' do
     get '/' do
+      puts request.env['HTTP_AUTHORIZATION']
       json Province.select('id', 'name').all
     end
 
     get '/:id' do
-      resource =  Province.find_by_id(params[:id])
+      resource =  Province.find(params[:id])
 
       if resource
         json resource
@@ -32,7 +41,7 @@ namespace '/v1' do
     end
 
     delete '/:id' do
-      resource = Province.find_by_id(params[:id])
+      resource = Province.find(params[:id])
 
       if resource
         resource.destroy
